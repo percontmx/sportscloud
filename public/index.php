bc308f76c93724360ec2913c78f9b803b0280b1e
Alex Percont <alex@softwarepercont.com>
2025-09-12 13:08:58 -0600

    Configuración básica para desplegar en Google Cloud App Engine.

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// public/index.php
+
+require __DIR__.'/../vendor/autoload.php';
+
+// Your application bootstrap logic starts here...
+echo "<html><body><h1>Welcome to SportsCloud</h1></body></html>";
