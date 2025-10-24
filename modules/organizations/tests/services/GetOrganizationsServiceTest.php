df24d4c0aeb5be21333d7146185d6e24fc4829b8
Alex Percont <alex@softwarepercont.com>
2025-10-23 22:06:45 -0600

    Servicio para obtener lista de organizaciones.

--- modules/organizations/tests/services/GetOrganizationsServiceTest.php
+++ modules/organizations/tests/services/GetOrganizationsServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Percontmx\SportsCloud\Organizations\Tests\Services;
 
+use Percontmx\SportsCloud\Organizations\Models\OrganizationsModel;
 use Percontmx\SportsCloud\Organizations\Tests\Support\Services\OrganizationsServiceTestBase;
 use PHPUnit\Framework\Attributes\Test;
 
@@ -17,5 +18,37 @@ class GetOrganizationsServiceTest extends OrganizationsServiceTestBase
         $this->assertNotNull($organization);
         $this->assertEquals('Federación Mexicana de Fútbol Asociación, A.C.', $organization->full_name);
         $this->assertEquals('Femexfut', $organization->short_name);
+    }
+
+    #[Test]
+    public function getOrganizations()
+    {
+        $service = service('organizations');
+        $organizations = $service->getListOfOrganizations();
+
+        $this->assertNotEmpty($organizations);
+        $this->assertCount(2, $organizations);
+    }
+
+    #[Test]
+    public function getOrganizationsNoDeleted() 
+    {
+        $model = model(OrganizationsModel::class);
+        $model->delete(1);
+        $service = service('organizations');
+        $organizations = $service->getListOfOrganizations(false);
+        $this->assertNotEmpty($organizations);
+        $this->assertCount(1, $organizations);
+    }
+
+    #[Test]
+    public function getOrganizationsWithDeleted()
+    {
+        $model = model(OrganizationsModel::class);
+        $model->delete(1);
+        $service = service('organizations');
+        $organizations = $service->getListOfOrganizations(true);
+        $this->assertNotEmpty($organizations);
+        $this->assertCount(2, $organizations);
     }
 }
